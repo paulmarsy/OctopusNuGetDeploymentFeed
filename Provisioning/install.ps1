1be2265535652d639a2d3c8d71d630ac10db44b3
Paul Marston <paul@marston.me>
2017-08-05 12:33:55 +0100

    Updating install script

--- Provisioning/install.ps1
+++ Provisioning/install.ps1
@@ -1,10 +1,12 @@
+param($Version = 'latest', $PrivateIp)
+
 $Account = 'paulmarsy'
 $Repo = 'OctopusNuGetDeploymentFeed'
 $BinaryName = 'OctopusDeployNuGetFeed.exe'
 $InstallDir = 'C:\OctopusDeployNuGetFeed'
 $AppFilePath = Join-Path $InstallDir $BinaryName
 
-$request = [System.Net.WebRequest]::Create("https://github.com/$Account/$Repo/releases/latest/")
+$request = [System.Net.WebRequest]::Create("https://github.com/$Account/$Repo/releases/$Version/")
 $request.AllowAutoRedirect = $false
 $response=$request.GetResponse()
 
@@ -14,10 +16,25 @@ if (!(Test-Path $InstallDir)) {
     New-Item -Path $InstallDir -ItemType Directory
 }
 
+if (Test-Path $AppFilePath) {
+    & $AppFilePath stop
+    if ($LASTEXITCODE -ne 0) { throw "$BinaryName did not stop" }
+
+    & $AppFilePath uninstall
+    if ($LASTEXITCODE -ne 0) { throw "$BinaryName did not uninstall" }
+}
+
+Write-Host "Downloading $downloadUri to $AppFilePath"
 Invoke-WebRequest -UseBasicParsing -Uri  $downloadUri -OutFile $AppFilePath
 
-& $AppFilePath install
+& $AppFilePath install -host:$PrivateIp
 if ($LASTEXITCODE -ne 0) { throw "$BinaryName did not install" }
 
 & $AppFilePath start
-if ($LASTEXITCODE -ne 0) { throw "$BinaryName did not start" }
+if ($LASTEXITCODE -ne 0) {
+    Write-Warning "$BinaryName did not start, retrying..."
+
+    Start-Sleep -Seconds 60
+    & $AppFilePath start
+    if ($LASTEXITCODE -ne 0) { throw "$BinaryName did not start" }
+}
