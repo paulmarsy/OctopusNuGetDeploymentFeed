199355eac568830d647c38b1b3a8f79f9ab2dffe
Paul Marston <paul@marston.me>
2017-09-05 02:22:14 +0100

    ok

--- OctopusDeployNuGetFeed/Services/NuGetFeed/NuGetFeedStartup.cs
+++ OctopusDeployNuGetFeed/Services/NuGetFeed/NuGetFeedStartup.cs
@@ -83,7 +83,7 @@ namespace OctopusDeployNuGetFeed.Services.NuGetFeed
 
         private static Task<bool> ShouldTrackRequest(IOwinContext owinContext)
         {
-            return Task.FromResult(string.Equals(owinContext.Request.Uri.AbsolutePath, "/lbprobe", StringComparison.OrdinalIgnoreCase) &&
+            return Task.FromResult(!string.Equals(owinContext.Request.Uri.AbsolutePath, "/lbprobe", StringComparison.OrdinalIgnoreCase) &&
                                    owinContext.Response.StatusCode != 401); // Avoid tracing '401 Forbidden' otherwise 50% of all requests show as failed requests
         }
 
